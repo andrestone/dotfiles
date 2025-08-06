7c782ba674eceee1e0411cb9b88dad8f0cbf71e5
André Fontenele <andrestone@users.noreply.github.com>
2025-08-06 17:54:47 +0200

    fix

--- install.sh
+++ install.sh
@@ -32,7 +32,6 @@ maybe_install_nvm() {
         return
     fi
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
     export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
     nvm install --lts
